2daa56f9fe47f14282b1c84a09353aa4a0d63469
Sujin Choi <sujin.2f@gmail.com>
2026-03-30 12:21:23 -0400

    update build commands

--- package.mjs
+++ package.mjs
@@ -12,11 +12,10 @@ await execPromise(`git submodule update --remote`)
 
 // Version & sudo
 const VERSION = process.env.npm_package_version
-const sudo = process.argv.indexOf('sudo') !== -1 ? 'sudo ' : ''
 
 // Check if Docker image exists
 let image = `sujin2f/auth:${VERSION}`
-const { stdout } = await execPromise(`${sudo}docker image ls ${image}`)
+const { stdout } = await execPromise(`docker image ls ${image}`)
 if (stdout.includes(image)) {
     console.error(`⛈️ Image ${image} already exists.`)
     process.exit(1)
@@ -53,7 +52,7 @@ const CRYPTO_KEY = generateCryptoKey()
 console.info(`👀 The crypto key is ${CRYPTO_KEY}. Apply this to @next, @graphql, and @wordpress`)
 
 console.log(`🤞 \x1B[32m- Creating Docker image ${image}... \x1B[0m`)
-exec(`${sudo}docker build -t ${image} .`, async (error, stdout, stderr) => {
+exec(`docker build -t ${image} .`, async (error, stdout, stderr) => {
     if (error) {
         console.error('🤬 \x1B[31m- docker build error: \x1B[0m', error)
         return
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -14,7 +14,7 @@ const config = {
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: [/node_modules/],
             },
         ],
     },
